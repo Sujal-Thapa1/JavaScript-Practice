e6658357b35f9a0b8615a2ca614613efa5b2f2dc
Sujal Thapa1 <sujalmangar304@gmail.com>
2025-01-13 22:17:38 +0530

    New Addition Task

---
+++ addition/script.js
@@ -0,0 +1,8 @@
+function addition(){
+    var num1,num2;
+    num1=parseInt(document.getElementById("num1").value);
+    num2=parseInt(document.getElementById("num2").value);
+    sum=num1+num2;
+    document.getElementById("output").value=sum;
+}
+console.log("Nice");
